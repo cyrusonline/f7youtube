ee03ed1ad159d7a89d5ede5292f34c435c8e2155
Cyrus Chan <cyruschan@hsmc.edu.hk>
2018-09-20 17:26:51 +0800

    add stream by own video on created

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,8 +4,7 @@
     <f7-block-title>{{ title }}</f7-block-title>
     <f7-block inner>
     <f7-button round @click="playvideo">Play</f7-button>
-
-    </f7-block>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/LdgoeVqZuFo?showinfo='0'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>    </f7-block>
   </f7-page>
 </template>
 <script>
@@ -19,7 +18,7 @@ export default {
   methods:{
     playvideo(){
            try {
-        window.InAppYouTube.openVideo('wRxhqhYlHBY', {
+        window.InAppYouTube.openVideo('LdgoeVqZuFo', {
           fullscreen: true
         }, function(result) {
           console.log(JSON.stringify(result));
@@ -32,6 +31,13 @@ export default {
     }
   },
   created(){
+
+    var videoUrl = 'http://techslides.com/demos/sample-videos/small.mp4';
+console.log('streaming above')
+  // Just play a video
+  window.plugins.streamingMedia.playVideo(videoUrl);
+  console.log('streaming below')
+
       //    try {
       //   window.InAppYouTube.openVideo('iTLrhcX_v6U', {
       //     fullscreen: true
